ff149dda8aeef93b2a349771a8c5fcb2fe27fdf8
luoweikingjj <luoweikingjj@gmail.com>
2020-10-01 22:37:40 +0800

    接口私有方法

---
+++ basic/src/main/java/com/example/basic/InterfacePrivateDemo.java
@@ -0,0 +1,22 @@
+package com.example.basic;
+
+public interface InterfacePrivateDemo {
+    default void func(){
+//        func1();
+//        func2();
+    }
+
+//    /**
+//     * java9 开始才有效
+//     */
+//    private void func1(){
+//        System.out.println("跑起来~~~");
+//    }
+
+//    /**
+//     * java9 开始才有效
+//     */
+//    private void func2(){
+//        System.out.println("跑起来~~~");
+//    }
+}
