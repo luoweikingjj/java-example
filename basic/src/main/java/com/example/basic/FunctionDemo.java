a2e99b897a8684b5b391ee9af080c7eaa16db22b
luoweikingjj <luoweikingjj@gmail.com>
2020-10-01 21:11:37 +0800

    方法

---
+++ basic/src/main/java/com/example/basic/FunctionDemo.java
@@ -0,0 +1,11 @@
+package com.example.basic;
+
+public class FunctionDemo {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    private static void method1(){
+        System.out.println("method1");
+    }
+}
