47f927ddd862ce72670d0e1377b312594cf141fa
luoweikingjj <luoweikingjj@gmail.com>
2020-10-03 14:35:45 +0800

    JDBC使用

---
+++ web/src/main/java/com/example/web/jdbcdemo/JDBCDemo.java
@@ -0,0 +1,187 @@
+package com.example.web.jdbcdemo;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.*;
+
+public class JDBCDemo {
+    public static void main(String[] args) throws SQLException {
+        // test();
+        // practice();
+        // resultSetDemo();
+        // preparedStatementDemo();
+        transactionDemo();
+    }
+
+    /**
+     * 测试
+     */
+    private static void test() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.10.10:3306/java-example", "homestead", "secret");
+
+            String sql = "update account set balance = 500 where id = 1";
+            Statement statement = connection.createStatement();
+            int count = statement.executeUpdate(sql);
+            System.out.println(count);
+
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 操作练习
+     */
+    private static void practice() {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://192.168.10.10:3306/java-example", "homestead", "secret");
+
+            String sql = "insert into account value (null, 10)";
+            statement = connection.createStatement();
+            int count = statement.executeUpdate(sql);
+            if (count > 0) {
+                System.out.println("添加成功");
+            } else {
+                System.out.println("添加失败");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 结果集
+     */
+    private static void resultSetDemo() {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://192.168.10.10:3306/java-example", "homestead", "secret");
+
+            String sql = "select * from account";
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                double balance = resultSet.getDouble("balance");
+                System.out.println(id + "|" + balance);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 预处理
+     */
+    private static void preparedStatementDemo() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://192.168.10.10:3306/java-example", "homestead", "secret");
+
+            String sql = "update account set balance = 100 where id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "1");
+            int count = preparedStatement.executeUpdate();
+            System.out.println(count);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 事务
+     */
+    private static void transactionDemo() {
+        Connection connection = null;
+        PreparedStatement preparedStatement1 = null;
+        PreparedStatement preparedStatement2 = null;
+        try {
+            connection = JDBCUtils.getConnection();
+            connection.setAutoCommit(false);
+
+            String sql1 = "update account set balance = balance - ? where id = ?";
+            String sql2 = "update account set balance = balance + ? where id = ?";
+
+            preparedStatement1 = connection.prepareStatement(sql1);
+            preparedStatement2 = connection.prepareStatement(sql2);
+
+            preparedStatement1.setDouble(1, 5);
+            preparedStatement1.setInt(2, 1);
+
+            preparedStatement2.setDouble(1, 5);
+            preparedStatement2.setInt(2, 2);
+
+            preparedStatement1.executeUpdate();
+
+            // 制造异常
+            int i = 3 / 0;
+
+            preparedStatement2.executeUpdate();
+
+            connection.commit();
+        } catch (Exception e) {
+            // 事务回滚
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(preparedStatement1, connection);
+            JDBCUtils.close(preparedStatement2, null);
+        }
+    }
+}
---
+++ web/src/main/java/com/example/web/jdbcdemo/JDBCUtils.java
@@ -0,0 +1,86 @@
+package com.example.web.jdbcdemo;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCUtils {
+    private static String url;
+    private static String user;
+    private static String password;
+    private static String driver;
+
+    static {
+        ClassLoader classLoader = JDBCUtils.class.getClassLoader();
+        URL resource = classLoader.getResource("jdbc.properties");
+        String path = resource.getPath();
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        url = properties.getProperty("url");
+        user = properties.getProperty("user");
+        password = properties.getProperty("password");
+        driver = properties.getProperty("driver");
+
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void close(Statement statement, Connection connection) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(ResultSet resultSet, Statement statement, Connection connection) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
